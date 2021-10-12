08323fb5c77c624aeb7e2b9079d3b8c47492a50e
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2021-10-12 11:24:04 +0900

    課題７途中

--- 3DLv1_00/GameProgramming/src/CModel.h
+++ 3DLv1_00/GameProgramming/src/CModel.h
@@ -1,5 +1,9 @@
 #ifndef CMODEL_H
 #define CMODEL_H
+//vectorのインクルード
+#include<vector>
+//CTriangleのインクルード
+#include"CTriangle.h"
 /*
 モデルクラス
 モデルデータの入力や表示
@@ -9,6 +13,10 @@ public:
 	//モデルファイルの入力
 	//Load(モデルファイル名、マテリアルファイル名）
 	void Load(char* obj, char* mtl);
+
+private:
+	//三角形の可変長配列
+	std::vector<CTriangle>mTriangles;
 
 };
 
