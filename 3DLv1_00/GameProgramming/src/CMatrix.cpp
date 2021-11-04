a60c3fb148f2c73981cce691a4500c19752822ba
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2021-11-04 11:42:32 +0900

    課題１１完成

--- 3DLv1_00/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_00/GameProgramming/src/CMatrix.cpp
@@ -6,7 +6,6 @@
 CMatrix::CMatrix() {
 	Identity();
 }
-
 void CMatrix::Print() {
 	printf("%10f %10f %10f %10f\n",
 		mM[0][0], mM[0][1], mM[0][2], mM[0][3]);
@@ -19,7 +18,10 @@ void CMatrix::Print() {
 }
 //’PˆÊs—ñ‚Ìì¬
 CMatrix CMatrix::Identity() {
-
+	mM[0][0] = 1, mM[0][1] = 0, mM[0][2] = 0, mM[0][3] = 0;
+	mM[1][0] = 0, mM[1][1] = 1, mM[1][2] = 0, mM[1][3] = 0;
+	mM[2][0] = 0, mM[2][1] = 0, mM[2][2] = 1, mM[2][3] = 0;
+	mM[3][0] = 0, mM[3][1] = 0, mM[3][2] = 0, mM[3][3] = 1;
 	//‚±‚Ìs—ñ‚ğ•Ô‚·
 	return *this;
 }
