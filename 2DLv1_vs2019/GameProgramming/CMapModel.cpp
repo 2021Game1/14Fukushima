8efab3c841d75abb8d087c3921a5185bf7b59097
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2021-12-20 13:41:23 +0900

    開発中

--- 2DLv1_vs2019/GameProgramming/CMapModel.cpp
+++ 2DLv1_vs2019/GameProgramming/CMapModel.cpp
@@ -25,20 +25,27 @@ void CMapModel::Load(char* csv, char* mtl) {
 	//ファイルから1行入力
 	//fgets(入力エリア,エリアサイズ,ファイルポインタ)
 	//ファイルの最後になるとNULLを返す
-	while (fgets(buf,sizeof(buf),fp)!= NULL){
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
 		//データを分割
-		int seq[8];
-		//文字列からデータを4つ変数へ代入する
-		//ssccanf(文字列,変換指定子,変数)
-		sscanf(buf,"%d, %d, %d, %d, %d, %d, %d, %d,",&seq[0],&seq[1],&seq[2],&seq[3],&seq[4],&seq[5],&seq[6],&seq[7]);
-		//数列の比較
-		//strcmp(列1,列2)
-		
-		if (seq[7] == 1);
-		
-		//入力した値をコンソールに出力する
-		printf("%s", buf);
-	}
+		int seq[9];
+		//数列からデータを9つ変数へ代入する
+		//ssccanf(数列,変換指定子,変数)
+		sscanf(buf, "%d, %d, %d, %d, %d, %d, %d, %d, %d,", &seq[0], &seq[1], &seq[2], &seq[3], &seq[4], &seq[5], &seq[6], &seq[7], &seq[8]);
+		for (int i = 0; i < 9; i++){
+				switch (seq[i]) {
+				case 0:
+					printf("%d\n", seq[i]);
+					break;
+				case 1:
+					printf("%d\n", seq[i]);
+					break;
+				case 2:
+					printf("%d\n", seq[i]);
+					break;
+				}
+			}
+			
+		}
 
 	//ファイルのクローズ
 	fclose(fp);
