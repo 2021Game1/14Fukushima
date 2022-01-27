3c133e3978d535264ae51986196b2d95298f1b64
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2022-01-27 12:29:01 +0900

    課題32完成

--- 3DLv1_00/GameProgramming/src/CCollider.cpp
+++ 3DLv1_00/GameProgramming/src/CCollider.cpp
@@ -49,6 +49,27 @@ bool CCollider::CollisionTriangleLine(CCollider* t, CCollider* l, CVector* a) {
 	//線分が面と交差しているか内積で確認する
 	float dots = v0sv.Dot(normal);
 	float dote = v0ev.Dot(normal);
+	//面と線分の交点を求める
+	//交点の計算
+	CVector cross = sv + (ev - sv) * (abs(dots)/(abs(dots) + abs(dote)));
+
+	//交点が三角形内なら衝突している
+	//頂点1頂点2ベクトルと頂点1交点ベクトル都の外積を求め、
+	//法線との内積がマイナスなら、三角形の外
+	if ((v[2] - v[1]).Cross(cross - v[1]).Dot(normal) < 0.0f) {
+		//衝突してない
+		*a = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+	//課題32
+//頂点3頂点1ベクトルと頂点2交点ベクトル外積を求め、
+//法線との内積がマイナスなら、三角形の外
+	if ((v[0] - v[2]).Cross(cross - v[2]).Dot(normal) < 0.0f) {
+		//衝突していない(調整不要)
+		*a = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+
 	//プラスは交差してない
 	if (dots * dote >= 0.0f)
 	{
