225bbf249a9ce9773e268b5b0dd9d9ab7b971094
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2022-07-26 11:16:47 +0900

    課題２２　提出用

--- 3DLv2_00_2022_vs2019/GameProgramming/src/CXPlayer.cpp
+++ 3DLv2_00_2022_vs2019/GameProgramming/src/CXPlayer.cpp
@@ -3,31 +3,40 @@
 
 
 void CXPlayer::Update() {
-
 	if (CKey::Once(' ')) {
 		ChangeAnimation(3, false, 30);
 	}
-	else if (IsAnimationFinished() && mAnimationIndex == 3){
+	else if (IsAnimationFinished() && mAnimationIndex == 3) {
 		ChangeAnimation(4, false, 30);
 	}
-	//Wキー入力で前進
-	if (CKey::Push('W') && !CKey::Push(' ')) {
-		mPosition = mPosition + (mMatrixRotate.VectorZ()* 0.1f);
-		ChangeAnimation(1,true,60);
-
-	}
-	else if (IsAnimationFinished() || mAnimationIndex == 1)
+	if (IsAnimationFinished())
 	{
 		ChangeAnimation(0, true, 60);
 	}
-	//Aキーを押すと２度回転
-	if (CKey::Push('A') && !CKey::Push(' ')) {
-		mRotation = mRotation + (mMatrixRotate.VectorY() * 2.0f);
+
+
+	if (mAnimationIndex == 1 || mAnimationIndex == 0)
+	{
+		//Wキー入力で前進
+		if (CKey::Push('W')) {
+			mPosition = mPosition + (mMatrixRotate.VectorZ() * 0.1f);
+			ChangeAnimation(1, true, 60);
+
+		}
+		else if (mAnimationIndex == 1)
+		{
+			ChangeAnimation(0, true, 60);
+		}
+		//Aキーを押すと２度回転
+		if (CKey::Push('A')) {
+			mRotation = mRotation + (mMatrixRotate.VectorY() * 2.0f);
+		}
+		//Dキーを押すと２度回転
+		if (CKey::Push('D')) {
+			mRotation = mRotation - (mMatrixRotate.VectorY() * 2.0f);
+		}
 	}
-	//Dキーを押すと２度回転
-	if (CKey::Push('D') && !CKey::Push(' ')) {
-		mRotation = mRotation - (mMatrixRotate.VectorY() * 2.0f);
-	}
+
 
 
 
